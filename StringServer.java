0d9f369365a7aa9f85ec245cd99e9c1580829476
Ramon Guerrero <rguerre2004@gmail.com>
2023-04-24 15:29:01 -0700

    commit

---
+++ StringServer.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.URI;
+
+class StringHandler implements URLHandler {
+    String s = "";
+
+    public String handleRequest(URI url){
+        if (url.getPath().equals("/")){
+            return String.format("Available paths: /add-message?s=(your string)");
+        }else{
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    s += parameters[1] + "\n";
+                    return s;
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class StringServer{
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
